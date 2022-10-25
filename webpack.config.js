ea20a5c3eb6de1308c19f2e9e024e34b8f42ad6a
Hidenori Miyoshi <miyohide@gmail.com>
2022-10-25 20:11:23 +0900

    change indent size

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 const path = require('path');
 
 module.exports = {
-    mode: "production",
-    entry: "./src/index.js",
-    watchOptions: {
-        ignored: /node_modules/
-    },
-    output: {
-        path: path.resolve(__dirname, 'public/js'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                            ],
-                        },
-                    },
-                ],
+  mode: "production",
+  entry: "./src/index.js",
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+              ],
             },
+          },
         ],
-    },
-    target: ["web", "es5"],
+      },
+    ],
+  },
+  target: ["web", "es5"],
 };
