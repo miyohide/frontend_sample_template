04a4e60f789a994f752be575b0c85ea3d33e3928
Hidenori Miyoshi <miyohide@gmail.com>
2022-10-26 20:58:48 +0900

    implement buildHtmlWebpackPlugins func

--- config/webpack/utils/entriesGenerator.js
+++ config/webpack/utils/entriesGenerator.js
@@ -1,9 +1,20 @@
 const glob = require("glob");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 exports.getEntries = (entryRoot) => {
-    let entries = {};
-    const filePaths = glob.sync(path.resolve(entryRoot, "*.js"))
-    filePaths.forEach(filePath => { entries[path.basename(filePath, path.extname(filePath))] = filePath} );
-    return entries;
+  let entries = {};
+  const filePaths = glob.sync(path.resolve(entryRoot, "*.js"))
+  filePaths.forEach(filePath => { entries[path.basename(filePath, path.extname(filePath))] = filePath });
+  return entries;
+}
+
+exports.buildHtmlWebpackPlugins = (entries, templateRootPath) => {
+  return Object.keys(entries, templateRootPath).map(
+    entryName => new HtmlWebpackPlugin({
+      filename: `${entryName}.html`,
+      template: `${templateRootPath}${entryName}.html`,
+      chunks: [entryName]
+    })
+  )
 }
