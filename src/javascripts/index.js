bfb99b5dcf37094a9ff6137670d044b5269daddb
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-09 21:01:41 +0900

    implement first axios program.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,6 +1,18 @@
 import { hello } from "./libs/hello";
+import axios from "axios";
 import "../stylesheets/styles.scss";
 
 const message = "Hello World";
 
 hello(message);
+
+document.getElementById('send_button').addEventListener('click', () => {
+  axios.get('http://localhost:8081/', {
+    params: {
+      name: message
+    }
+  }).then(function(response) {
+    console.log(response.data);
+    document.getElementById('backendValue').innerText = response.data.content;
+  });
+});
