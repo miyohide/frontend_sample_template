86282622cc4d725df8216b3bed9305c3fbe3a920
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-12 21:05:57 +0900

    create apicall file

---
+++ src/javascripts/libs/apicall.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const getRootPathWithName = async (message) => {
+  axios.defaults.baseURL = 'http://localhost:8080';
+  let rval = "";
+  await axios.get('/', {
+    params: {
+      name: message
+    }
+  }).then(function(response) {
+    rval = response.data;
+  });
+  return rval;
+}
