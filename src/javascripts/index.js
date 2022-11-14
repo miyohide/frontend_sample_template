9cd721b7b71d4426822f8e2f8b740abc3cfa67dd
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-14 18:51:16 +0900

    delete hello

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,10 +1,8 @@
 import "../stylesheets/styles.scss";
 import { getRootPathWithName } from "./libs/apicall";
-import { hello } from "./libs/hello";
 
 document.getElementById('send_button').addEventListener('click', async () => {
   const message = document.getElementById("hello_name").value;
-  console.log(hello(message));
   const backendValue = await getRootPathWithName(message)
   console.log(backendValue);
   document.getElementById('backendValue').innerText = backendValue.content;
