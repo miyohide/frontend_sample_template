368274cf673b8dda2cdaa6af3b8a50a08207ce2c
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-15 21:19:13 +0900

    delete unused file

--- __tests__/libs/hello.test.js
+++
@@ -1,5 +0,0 @@
-const hello = require("../../src/javascripts/libs/hello");
-
-test("return hello message", () => {
-  expect(hello.hello("abc")).toBe("abcを出力しました");
-});
--- src/javascripts/libs/hello.js
+++
@@ -1,4 +0,0 @@
-export const hello = (message) => {
-  console.log(`${message}を出力しました`);
-  return `${message}を出力しました`;
-};
