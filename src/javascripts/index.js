ecb9f6c6ba1136cc0d08433ce90a7979842ff32a
Hidenori Miyoshi <miyohide@gmail.com>
2022-11-18 21:10:54 +0900

    delete debug log

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,6 +4,5 @@ import { getRootPathWithName } from "./libs/apicall";
 document.getElementById("send_button").addEventListener("click", async () => {
   const message = document.getElementById("hello_name").value;
   const backendValue = await getRootPathWithName(message);
-  console.log(backendValue);
   document.getElementById("backendValue").innerText = backendValue.content;
 });
