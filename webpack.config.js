296e378b9a330c0c6ee356cdd1974d7e38eecedc
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-21 20:27:00 +0900

    create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/javascripts/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './javascripts/index.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      filename: 'index.html',
+      template: './src/htmls/index.html',
+      chunks: ['index'],
+    }),
+  ],
+};
