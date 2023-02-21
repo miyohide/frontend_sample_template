bd7bdd71305628d3ddccb55fb6f651e5defed68d
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-21 20:10:42 +0900

    refresh

--- src/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-import "../stylesheets/styles.scss";
-import { getWelcomeMessage } from "./libs/apicall";
-
-document.getElementById("send_button").addEventListener("click", async () => {
-  const message = document.getElementById("hello_name").value;
-  const backendValue = await getWelcomeMessage(message);
-  document.getElementById("backendValue").innerText = backendValue.content;
-});
