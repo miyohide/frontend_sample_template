458fa670e7a1802d382b8f6763615e528359b7aa
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-24 06:51:13 +0900

    add comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
 
+// 対象パスのJavaScriptファイルを検索し、キーとしてRootからの相対パス＋ファイル名（拡張子なし）、
+// 値として、ファイル名のMapを生成する。
 const entries = WebpackWatchedGlobEntries.getEntries([path.resolve(__dirname, './src/javascripts/**/*.js')], {
   ignore: path.resolve(__dirname, './src/javascripts/**/_*.js'),
 })();
 
+// 与えられたentriesとHTMLが格納されているパスをもとにHtmlWebpackPluginを
+// 動的に生成する
 const htmlGlobPlugins = (entries, srcPath) => {
   return Object.keys(entries).map((key) =>
     new HtmlWebpackPlugin({
