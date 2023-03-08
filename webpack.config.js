59c13d59aeb35340481359e2f2ae8f0d340cc9a4
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-09 06:50:38 +0900

    add scss config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // 対象パスのJavaScriptファイルを検索し、キーとしてRootからの相対パス＋ファイル名（拡張子なし）、
 // 値として、ファイル名のMapを生成する。
@@ -33,8 +34,22 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: './javascripts/[name].js',
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
     new CleanWebpackPlugin(),
     ...htmlGlobPlugins(entries, './src/htmls')
   ],
